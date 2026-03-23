20a98b928616d7164006fecd2834a695a2271b77
Hasan Zahid <01-131232-028@student.bahria.edu.pk>
2026-03-24 00:17:47 +0500

    Rename Test1.cs to test.cs and lowercase class name
    Rename the file selenium/Test1.cs to selenium/test.cs and update the class name from Test1 to test (case change). No other code changes; keeps implementation intact while adjusting naming/casing for consistency.

--- selenium/test.cs
+++ selenium/test.cs
@@ -9,7 +9,7 @@ using System.Threading;
 namespace selenium
 {
     [TestClass]
-    public class Test1
+    public class test
     {
         private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
         protected static WindowsDriver<WindowsElement> session;
