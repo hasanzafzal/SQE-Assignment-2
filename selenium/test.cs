54de5307a0e67304d2187c699759027d1969db13
Hasan Zahid <01-131232-028@student.bahria.edu.pk>
2026-03-24 11:42:47 +0500

    Update test.cs

--- selenium/test.cs
+++ selenium/test.cs
@@ -24,6 +24,7 @@ namespace selenium
             options.AddAdditionalCapability("app", appPath);
         
             session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), options);
+            session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             Assert.IsNotNull(session);
         }
 
